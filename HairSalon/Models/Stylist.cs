48052a065a39ee7f359682b62ebfe7b23b2cb80b
charley mcgowan <charley.mcgowan@outlook.com>
2018-12-07 13:11:24 -0800

    Test GetClients_ReturnsAList_True passes successsfully

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -110,27 +110,26 @@ namespace HairSalon.Models
 
         public List<Client> GetClients()
         {
-            Client dummyClient = new Client("Jimmy", 1);
-            List<Client> allStylistClients = new List<Client> {dummyClient};
-            //MySqlConnection conn = DB.Connection();
-            //conn.Open();
-            //var cmd = conn.CreateCommand() as MySqlCommand;
-            //cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylist_id;";
-            //cmd.Parameters.AddWithValue("@stylist_id", this.id);
-            //var rdr = cmd.ExecuteReader() as MySqlDataReader;
-            //while (rdr.Read())
-            //{
-            //    int clientId = rdr.GetInt32(0);
-            //    string clientDescription = rdr.GetString(1);
-            //    int clientStylistId = rdr.GetInt32(2);
-            //    Client newClient = new Client(clientDescription, clientStylistId, clientId);
-            //    allStylistClients.Add(newClient);
-            //}
-            //conn.Close();
-            //if (conn != null)
-            //{
-            //    conn.Dispose();
-            //}
+            List<Client> allStylistClients = new List<Client> {};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylist_id;";
+            cmd.Parameters.AddWithValue("@stylist_id", this.id);
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int clientId = rdr.GetInt32(0);
+                string clientDescription = rdr.GetString(1);
+                int clientStylistId = rdr.GetInt32(2);
+                Client newClient = new Client(clientDescription, clientStylistId, clientId);
+                allStylistClients.Add(newClient);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
             return allStylistClients;
         }
     }
