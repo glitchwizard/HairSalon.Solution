7e93c911bb43a32d3b149235ec6b502ea0416f3b
charley mcgowan <charley.mcgowan@outlook.com>
2018-12-07 11:26:44 -0800

    Model Tests finished

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -12,24 +12,4 @@ namespace HairSalon.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
-    }
 }
